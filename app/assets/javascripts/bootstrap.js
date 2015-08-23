aef2cfd597c9df4da04cc92d829a2d428e5a160e
spongah <rocksolid801@gmail.com>
2015-08-22 20:04:56 -0600

    took out silly bootstrap.js addition, trying from scratch to see how things behave

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -8,9 +8,6 @@ if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
 
-$(document).ready(function() {
-    $(".dropdown-toggle").dropdown();
-});
 
 +function ($) {
   'use strict';
