8d1de686da2e150686aaade5a6e3f81135c8f9d2
spongah <rocksolid801@gmail.com>
2015-08-26 13:26:27 -0600

    added mandrill-api to gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,11 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
   config.action_mailer.default_url_options = { :host => 'soundresume.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
@@ -21,6 +26,7 @@ Rails.application.configure do
   :port      => 587,
   :user_name => ENV["MANDRILL_USERNAME"],
   :password  => ENV["MANDRILL_API_KEY"]
+
 }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
