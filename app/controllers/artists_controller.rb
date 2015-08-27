c4094d2f8c23d9195fc03f96d86b1341bbdc2a74
spongah <rocksolid801@gmail.com>
2015-08-27 04:23:14 -0600

    fixed a small oversight pertaining to admin access'

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,7 +4,7 @@ class ArtistsController < ApplicationController
   # GET /artists
   # GET /artists.json
   def index
-    @artists = Artist.all
+    @artists = Artist.all.order(:genre)
   end
 
   # GET /artists/1
