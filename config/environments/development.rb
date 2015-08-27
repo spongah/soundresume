6f2bc9e9e89bb9a26446fd4b371fd98b57ee1b41
spongah <rocksolid801@gmail.com>
2015-08-27 03:21:02 -0600

    added confirmable to devise, modified error messages so they flash, they arent perfect but they all show now, and emails should all work.. will need to drop create and migrate database on heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.default_url_options = { :host => 'soundresume.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   config.action_mailer.smtp_settings = {
   :address   => "smtp.mandrillapp.com",
