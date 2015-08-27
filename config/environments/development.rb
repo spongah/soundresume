6b9935e68ebfe7c561d0a7975c2257b93188e8d1
spongah <rocksolid801@gmail.com>
2015-08-27 00:38:15 -0600

    oops, using APIKEY instead of API_KEY

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   :address   => "smtp.mandrillapp.com",
   :port      => 587,
   :user_name => ENV["MANDRILL_USERNAME"],
-  :password  => ENV["MANDRILL_API_KEY"],
+  :password  => ENV["MANDRILL_APIKEY"],
   :domain => 'herokuapp.com',
   :authentication => :plain,
 
